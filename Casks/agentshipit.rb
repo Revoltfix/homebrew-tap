296537f2661f7f4a9f12bcfd6b1639b3b73adffd
İbrahim Ordo <ibrahimordo@172.16.32.81>
2026-03-22 01:41:35 +0300

    Update Agentshipit to v3.2.0

--- Casks/agentshipit.rb
+++ Casks/agentshipit.rb
@@ -1,6 +1,6 @@
 cask "agentshipit" do
-  version "3.0.0"
-  sha256 "9a6cf4a66edd2c6c046549b8e985591ece29dfe3b63ba4ebb555f53ede82c1b5"
+  version "3.2.0"
+  sha256 "3bafee6224dc26bbf4e1249413b1adfc17e7e716e3da08aa6c3bd9b5a512539b"
 
   url "https://github.com/Revoltfix/agentshipit/releases/download/v#{version}/AgentShipIt_#{version}_aarch64.dmg"
   name "AgentShipIt"
