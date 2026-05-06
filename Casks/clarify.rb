45c9e3256421c962699ef84a95958e438cda5640
Yusuf Çalışkan <yusufocaliskan@gmail.com>
2026-05-06 18:11:04 +0300

    Update Clarify to v1.3.1

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -1,6 +1,6 @@
 cask "clarify" do
-  version "1.3.0"
-  sha256 "304bd7bb9d273fc3de51ed1f730db97a2e3e7d3a9d9428955a1c485d84c827da"
+  version "1.3.1"
+  sha256 "f1db33cef9d3ddf1473d50b726b05bbb706ff3f41cc822c6f20d9608dd6d1ebc"
 
   url "https://github.com/Revoltfix/clarify/releases/download/v#{version}/Clarifyer_#{version}_aarch64.dmg"
   name "Clarifyer"
